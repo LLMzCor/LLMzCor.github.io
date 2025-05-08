79cd2ad19b8abe9ff64cc72241477995c6e55094
sana-lr <juan.robledo+mzcor@unc.edu.ar>
2025-05-07 22:58:17 -0300

    Bacteria prompt list

--- lista_promtps.py
+++ lista_promtps.py
@@ -12,4 +12,39 @@ prompt_2= """Following the abstrac of this paper: {question}, classify it into t
                                                 3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
                                                 4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
                                                 Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
+                                                """                                     
+prompt_Ct= """Following the abstrac of this paper bellows to Chlamidia trachomatis: {question}, classify it into the appropriate category based on the provided descriptions: 
+                                                1.  Multiresistance bacteria stains report,
+                                                2.  New Treatments,
+                                                3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
+                                                4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
+                                                Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
+                                                """  
+prompt_Hi= """Following the abstrac of this paper bellows to Haemopilus influenzae: {question}, classify it into the appropriate category based on the provided descriptions: 
+                                                1.  Multiresistance bacteria stains report,
+                                                2.  New Treatments,
+                                                3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
+                                                4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
+                                                Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
                                                 """                                           
+prompt_Kp= """Following the abstrac of this paper bellows to Klebsiella pneumoniae: {question}, classify it into the appropriate category based on the provided descriptions: 
+                                                1.  Multiresistance bacteria stains report,
+                                                2.  New Treatments,
+                                                3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
+                                                4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
+                                                Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
+                                                """  
+prompt_Ng= """Following the abstrac of this paper bellows to Neisseria gonorrhoeae: {question}, classify it into the appropriate category based on the provided descriptions: 
+                                                1.  Multiresistance bacteria stains report,
+                                                2.  New Treatments,
+                                                3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
+                                                4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
+                                                Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
+                                                """  
+prompt_Sh= """Following the abstrac of this paper bellows to Shigela spp: {question}, classify it into the appropriate category based on the provided descriptions: 
+                                                1.  Multiresistance bacteria stains report,
+                                                2.  New Treatments,
+                                                3.  Immunization: It involves directly stimulating the immune system to generate immunological memory against a pathogen,
+                                                4.  None: The paper does not discuss Multiresistance bacteria stains report, neither new treatments nor immunization.
+                                                Your response should be formated as a list of 2 elements, stating the category number or categories selected followed by a concise explanation that supports your categorization.
+                                                """  
